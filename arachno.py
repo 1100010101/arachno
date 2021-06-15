f909be1817bca67b1d3721c8ffef111725b8ca7a
Noel Kangas <kangasnoel1@gmail.com>
2021-06-15 17:05:40 +0300

    tein neliön

--- arachno.py
+++ arachno.py
@@ -36,6 +36,14 @@ pygame.init()
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
 
+#green color
+
+green = (0,128,0)
+
+#rectangle
+
+nelio = pygame.Rect(50, 50, 100, 100)
+
 # Run until the user asks to quit
 running = True
 while running:
@@ -50,6 +58,8 @@ while running:
 
     # Draw a solid blue circle in the center
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.draw.rect(screen, green, nelio )
+    
 
     # Flip the display
     pygame.display.flip()
