97d5ee58bf09a3d74c6f3e96688b0fa37324a5e2
Noel Kangas <kangasnoel1@gmail.com>
2021-10-03 18:28:10 +0300

    koodia

--- arachno_arcade.py
+++ arachno_arcade.py
@@ -2,6 +2,8 @@
 
 
 import arcade
+import random
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -16,17 +18,50 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.WHITE)
 
     def setup(self):
-        # Set up your game here
-        pass
+        """ Set up the game and initialize the variables. """
 
-    def on_draw(self):
-        """ Render the screen. """
-        arcade.start_render()
-        # Your drawing code goes here
+        SPRITE_SCALING_FLY = 0.6
+        SPRITE_SCALING_SPIDER = 1.2
 
-    def update(self, delta_time):
-        """ All the logic to move, and the game logic goes here. """
-        pass
+        FLY_COUNT = 10
+
+        # Create the sprite lists
+        self.spider_list = arcade.SpriteList()
+        self.fly_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
+        # Set up the player
+        # Character image from kenney.nl
+        self.spider_sprite = arcade.Sprite("images/spider.png", SPRITE_SCALING_SPIDER)
+        self.spider_sprite.center_x = 50 # Starting position
+        self.spider_sprite.center_y = 50
+        self.spider_list.append(self.spider_sprite)
+
+        # Create the coins
+        for i in range(FLY_COUNT):
+
+            # Create the fly instance
+            
+            fly = arcade.Sprite("images/fly.png", SPRITE_SCALING_FLY)
+
+            # Position the fly
+            fly.center_x = random.randrange(SCREEN_WIDTH)
+            fly.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the fly to the lists
+            self.fly_list.append(fly)
+            pass
+
+        def on_draw(self):
+            """ Render the screen. """
+            arcade.start_render()
+            # Your drawing code goes here
+
+        def update(self, delta_time):
+            """ All the logic to move, and the game logic goes here. """
+            pass
 
 
 def main():
@@ -37,3 +72,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
